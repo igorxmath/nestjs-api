90428239341967c07466a2a246e46c9f72568a60
igormxath <pessoal@igormatheus.com.br>
2023-01-30 12:16:20 -0300

    add exceptions messages

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,23 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/signin.dto';
 import { SignUpDto } from './dto/signup.dto';
+import { HttpExceptionFilter } from './exception/http-exception.filter';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseFilters,
+} from '@nestjs/common';
 
 @Controller('auth')
+@UseFilters(new HttpExceptionFilter())
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @HttpCode(HttpStatus.OK)
   @Post('signin')
+  @HttpCode(HttpStatus.OK)
   async signin(@Body() signInDto: SignInDto) {
     return this.authService.signin(signInDto);
   }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UserRepository } from 'src/repositories/user-repository';
 import { SignInDto } from './dto/signin.dto';
 import { SignUpDto } from './dto/signup.dto';
@@ -15,17 +15,25 @@ export class AuthService {
   ) {}
 
   async signin({ email, password }: SignInDto) {
-    const user = await this.userRepository.find(email);
-    const isMatched = await bcrypt.compare(password, user.password);
-    if (user && isMatched) {
-      return this.token(user.id, email);
+    try {
+      const user = await this.userRepository.find(email);
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (user && isMatch) {
+        return this.token(user.id, email);
+      } else throw new Error();
+    } catch (error) {
+      throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
     }
   }
 
   async signup({ name, email, password }: SignUpDto) {
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const user = await this.userRepository.create(name, email, hashedPassword);
-    return this.token(user.id, email);
+    const hashPassword = await bcrypt.hash(password, 12);
+    try {
+      const user = await this.userRepository.create(name, email, hashPassword);
+      return this.token(user.id, email);
+    } catch (error) {
+      throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+    }
   }
 
   async token(id: string, email: string): Promise<{ access_token: string }> {
